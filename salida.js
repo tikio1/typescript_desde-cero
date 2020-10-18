9f583c5115a17a2e5bc7c8ccce9861abf2b3da7b
tikio1 <eutiquio1000@gmail.com>
2020-10-18 11:02:20 -0500

    Primer commit en typescript

---
+++ salida.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+var myString = "hello world";
+myString = 22 + "";
+myString.split("");
+var mynumber = 22;
+var mybool = true;
+var myVar = "hello";
+myVar = 22;
---
+++ salida.ts
@@ -0,0 +1,10 @@
+console.log("hello world");
+var myString: string = "hello world";
+myString = 22 + "";
+
+var mynumber: number = 22;
+
+var mybool: boolean = true;
+
+var myVar: any = "hello";
+myVar = 22;
